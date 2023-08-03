faba098cff0194e6beb7f2f9e9ff5f0fbce9b225
RafaelFrire <rafaelfesousa@hotmail.com>
2023-08-03 11:38:08 -0300

    primeiro commit

---
+++ src/components/Generator.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { Title, Maindiv, Password, BtnGenerator, InputLength } from './components'
+
+
+function generator() {
+
+    const[password, setPassword] = React.useState(null)
+    const[length, setLength] = React.useState(0)
+
+    const CreatePass = () =>{
+        const charset = ['a', 'b', 'c', 'd', 'E', 'F', 'G','h', 'i','j', 1, 2, 3, 4, 5, '!', '@', '$', '&'];
+        let List = []
+
+
+        let i
+        for(i=0; i<=parseInt(length) -1 ; i++){
+            const randomNumber = Math.floor(Math.random()*charset.length)
+            List.push(charset[randomNumber])
+        }
+        // Tratando o array
+        const convertArray = List.toString();
+        const ConvertedArray = convertArray.replaceAll(',','')
+        setPassword(ConvertedArray)
+    }
+
+    const handleChange = (event)=>{
+        // Quantidade de caracteres que serao gerados
+        const value = event.target.value
+        setLength(parseInt(value))
+    }
+
+    useEffect(()=>{
+        CreatePass();
+    },[length])
+
+
+  return (
+    <div>
+        <Title>Gerador de Senhas</Title>
+        <Maindiv>
+            <Password>{password}</Password>
+             <InputLength type='range' min='0' max='20' onChange={handleChange}/>
+             Charset:{length}
+             {<CopyToClipboard text={password}>
+                <BtnGenerator>Copiar</BtnGenerator>
+            </CopyToClipboard> }
+            
+        </Maindiv>
+
+    </div>
+  )
+}
+
+export default generator;
---
+++ src/components/components.jsx
@@ -0,0 +1,64 @@
+import style from 'styled-components';
+
+
+
+export const Title = style.h1`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-weight: 500;
+    font-family: sans-serif;
+    color: Black;
+    min-height: 15vh;
+    font-size: 3.2em;
+`
+
+export const Maindiv = style.div`
+    max-width: 400px;
+    min-height: 30vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    background: white;
+    border-radius: 5px;
+    box-shadow: 0px 1px 2px black;
+    margin: 0px auto;
+`
+
+export const Password = style.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: none;
+    font-family: monospace;
+    border: 2px solid #1684FF;
+    border-radius: 2px;
+    font-size: 1.8em;
+    color: Black;
+    width: 80%;
+    min-height: 5vh;
+ 
+
+   
+
+   
+`
+
+export const InputLength = style.input`
+    width:200px;
+    
+`
+
+export const BtnGenerator = style.button`
+    width: 200px;
+    height: 40px;
+    cursor: pointer;
+    font-weight: 100;
+    font-size: 1.3em;
+    font-family: sans-serif;
+    border:1px solid grey;
+    background-color: #1684FF;
+    color: white;
+`
